fa34bdfb10e12efa71d9042e49ca0d2832e81617
farukulWD <farukgb1999@gmail.com>
2024-06-15 21:16:53 +0600

    schema correction

--- src/app/modules/products/productModel.js
+++ src/app/modules/products/productModel.js
@@ -1,16 +1,36 @@
 import { Schema } from "mongoose";
 
-export const productSchema = new Schema({
-  code: {
-    type: String,
-    require: true,
+export const productSchema = new Schema(
+  {
+    code: {
+      type: String,
+      require: true,
+    },
+    name: {
+      type: String,
+      require: true,
+    },
+    groupName: {
+      type: String,
+      require: true,
+    },
+    price: {
+      type: Number,
+      require: true,
+    },
+    company: {
+      type: String,
+      require: true,
+    },
+
+    netPrice: {
+      type: String,
+      require: true,
+    },
   },
-  name: {
-    type: String,
-    require: true,
-  },
-  groupName: {
-    type: String,
-    require: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+export const Product = model("Product", productSchema);
