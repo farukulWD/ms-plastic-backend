850442740944e1dd2485ac4131aa31e1f5486548
farukulWD <farukgb1999@gmail.com>
2024-06-24 00:08:56 +0600

    make delete cart api

--- src/app/modules/cart/cartRoute.js
+++ src/app/modules/cart/cartRoute.js
@@ -4,5 +4,7 @@ import { CartControllers } from "./cartController.js";
 const router = Router();
 
 router.post("/add-cart", CartControllers.addCart);
+router.get("/get-carts", CartControllers.getCarts);
+router.delete("/delete-cart", CartControllers.deleteCart);
 
 export const cartRoutes = router;
--- src/app/modules/cart/cartService.js
+++ src/app/modules/cart/cartService.js
@@ -6,19 +6,43 @@ const makeCartIntoDB = async (userId, products) => {
   if (!userId) {
     throw new Error("User Id is required");
   }
-  if (products.length < 0) {
+  if (products.length === 0) {
     throw new Error("Product can not be empty");
   }
 
-  const user = User.findOne({ _id: new ObjectId(userId) });
-  if (!user) {
+  const userFind = await User.findOne({ _id: new ObjectId(userId) });
+  if (!userFind) {
     throw new Error("User not found");
   }
-  const data = { userId, products };
+  const data = { user: userId, products };
   const result = await Cart.create(data);
+  return result;
+};
+
+const getCartsFromDB = async () => {
+  const result = await Cart.find({})
+    .populate("products.product")
+    .populate("user");
+  return result;
+};
+
+const deleteCartFromDB = async (cartId) => {
+  if (!cartId) {
+    throw new Error("Cart Id is required");
+  }
+
+  const findCart = await Cart.findOne({ _id: new ObjectId(cartId) });
+
+  if (!findCart) {
+    throw new Error("Cart not found");
+  }
+
+  const result = await Cart.findOneAndDelete({ _id: new ObjectId(cartId) });
   return result;
 };
 
 export const CartServices = {
   makeCartIntoDB,
+  getCartsFromDB,
+  deleteCartFromDB,
 };
