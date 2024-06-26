aea8ede280b69c7932eacddcd38debfd760c8f15
farukulWD <farukgb1999@gmail.com>
2024-06-26 22:05:49 +0600

    make global error handler

---
+++ src/app/utils/catchAsync.js
@@ -0,0 +1,8 @@
+const catchAsync = (fn) => {
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch((error) => {
+      return next(error);
+    });
+  };
+};
+export default catchAsync;
